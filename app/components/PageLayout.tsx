a9eb0331b847b04aa14edf7967119515b04a97fc
bozdoz <howaboutben@gmail.com>
2021-05-24 02:55:10 -0300

    adds basic centered layout

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -1,7 +1,18 @@
 import styled from "styled-components";
 
+const breakpoint = "600px";
+
 const StyledPage = styled.div`
-  background: #def;
+  display: grid;
+  grid-template-columns: 0 1fr 0;
+
+  > * {
+    grid-column: 2 / span 1;
+  }
+
+  @media (min-width: ${breakpoint}) {
+    grid-template-columns: 1fr ${breakpoint} 1fr;
+  }
 `;
 
 const Page: React.FC = ({ children }) => <StyledPage>{children}</StyledPage>;
