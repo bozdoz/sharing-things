e9dfc560425b03b12846631ae7d9bf2184b547b1
bozdoz <howaboutben@gmail.com>
2021-05-25 18:10:17 -0300

    removes hydration diffs

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,23 +4,34 @@ import Head from "next/head";
 import "../styles/globals.scss";
 import useDarkMode from "../hooks/useDarkMode";
 
+/** disable all server-side rendering mismatches */
+const SafeHydrate: React.FC = ({ children }) => {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+};
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [darkMode, setDarkMode] = useDarkMode();
 
   return (
-    <Page>
-      <Head>
-        <title>Sharing Things</title>
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐝</text></svg>"
-        />
-      </Head>
-      <button type="button" onClick={() => setDarkMode(!darkMode)}>
-        🌙 Toggle Dark Mode
-      </button>
-      <Component {...pageProps} />
-    </Page>
+    <SafeHydrate>
+      <Page>
+        <Head>
+          <title>Sharing Things</title>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐝</text></svg>"
+          />
+        </Head>
+        <button type="button" onClick={() => setDarkMode(!darkMode)}>
+          🌙 Toggle Dark Mode
+        </button>
+        <Component {...pageProps} />
+      </Page>
+    </SafeHydrate>
   );
 };
 
