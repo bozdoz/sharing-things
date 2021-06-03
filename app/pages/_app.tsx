93fe3b8790537dd7254f888c636359989dd154ed
bozdoz <howaboutben@gmail.com>
2021-06-03 10:38:05 -0300

    waffles make more sense

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -20,7 +20,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           <title>Sharing Things</title>
           <link
             rel="icon"
-            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐝</text></svg>"
+            href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧇</text></svg>"
           />
         </Head>
         <Component {...pageProps} />
