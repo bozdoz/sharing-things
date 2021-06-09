c4bf6a4d45ce5ae739a201252d86890a700938f3
bozdoz <howaboutben@gmail.com>
2021-06-08 23:05:52 -0300

    adds revalidating to ThingList

--- app/components/ThingList.tsx
+++ app/components/ThingList.tsx
@@ -1,8 +1,10 @@
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import { useRouter } from "next/router";
 import { Thing } from "models/types";
 import api from "resources/api";
 import ThingComponent from "./ThingComponent";
+import useSocket from "hooks/useSocket";
+import { useEffect } from "react";
 
 interface APIResponse {
   success: boolean;
@@ -11,10 +13,24 @@ interface APIResponse {
 
 const ThingList: React.FC = () => {
   const router = useRouter();
-  const { data, error } = useSWR<APIResponse>(
-    `/api/v1/thing/list?namespace=${router.asPath}`,
-    api
-  );
+  const socket = useSocket();
+  const url = `/api/v1/thing/list?namespace=${router.asPath}`;
+  const { data, error, isValidating } = useSWR<APIResponse>(url, api);
+
+  // TODO: refactor this to be useSocketEffect or something more DRY
+  useEffect(() => {
+    if (socket) {
+      const cb = () => {
+        mutate(url);
+      };
+
+      socket.on("refresh things", cb);
+
+      return () => {
+        socket.off("refresh things", cb);
+      };
+    }
+  }, [socket, url]);
 
   if (!data) {
     // TODO: skeleton
@@ -27,11 +43,12 @@ const ThingList: React.FC = () => {
   }
 
   return (
-    <>
+    // TODO: loading style
+    <div className={(isValidating && "loading") || undefined}>
       {data.data.map((props) => (
         <ThingComponent key={props._id} {...props} />
       ))}
-    </>
+    </div>
   );
 };
 
