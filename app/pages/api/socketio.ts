1e6118622f963e706a577454e99c06a5bb92d09c
bozdoz <howaboutben@gmail.com>
2021-06-10 02:32:11 -0300

    adds thing-stealing and thing-invalidation

--- app/pages/api/socketio.ts
+++ app/pages/api/socketio.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Socket } from "net";
+import { Server as HttpServer } from "http";
 import { NextApiRequest, NextApiResponse } from "next";
-import updateUser from "server-utils/updateUser";
 import { Server } from "socket.io";
+import dbConnect from "server-utils/dbConnect";
+import Thing from "models/Thing";
+
+interface CustomServer extends HttpServer {
+  io?: Server;
+}
 
 interface CustomSocket extends Socket {
-  server: {
-    io?: Server;
-  };
+  server: CustomServer;
 }
 
 interface CustomResponse extends NextApiResponse {
@@ -15,66 +19,118 @@ interface CustomResponse extends NextApiResponse {
 }
 
 /** a map of userIds and connection counts */
-const users: Record<string, number> = {};
+const userCounts: Record<string, number> = {};
 
 const socketIo = (_req: NextApiRequest, res: CustomResponse) => {
-  // cache the web socket server if in production
+  // cache the web socket server
   if (!res.socket.server.io) {
-    // no idea what type this is supposed to be
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const server: any = res.socket.server;
+    const server = res.socket.server;
     const io = new Server(server);
 
+    // TODO: extract some of this
+    // TODO: move event strings to constants
     io.on("connection", (socket) => {
       /** broadcast count of *other* active users */
       const updateActiveUsers = () => {
-        socket.broadcast.emit(
+        io.sockets.emit(
           "active users",
-          Object.keys(users).filter((userId) => userId !== socket.data.userId)
-            .length
+          Object.keys(userCounts).filter(
+            (userId) => userId !== socket.data.userId
+          ).length
         );
       };
 
-      // update db to make user active
-      socket.on("set active user", (userId: string) => {
-        // set userId to the session
-        socket.data.userId = userId;
+      /** refresh Thing queries for a given namespace */
+      const refreshThings = (namespace: string[]) => {
+        io.to(namespace).emit("refresh things");
+      };
 
-        // increment connection count for user
-        users[userId] = (users[userId] || 0) + 1;
+      socket.on(
+        "set active user",
+        ({ userId, namespace }: { userId: string; namespace: string }) => {
+          // set userId to the session
+          socket.data.userId = userId;
 
-        console.log("set active", users);
+          // make user join its own room
+          // TODO: make userId identical to socket.id
+          socket.join(userId);
+          socket.join(namespace);
 
-        // initial connection updates database
-        if (users[userId] === 1) {
-          updateUser(userId, {
-            active: true,
-          });
+          // increment connection count for user
+          userCounts[userId] = (userCounts[userId] || 0) + 1;
 
-          updateActiveUsers();
+          // initial connection
+          if (userCounts[userId] === 1) {
+            updateActiveUsers();
+          }
         }
-      });
+      );
 
-      // update db to make user inactive
-      socket.on("disconnect", (reason) => {
+      // make user inactive
+      socket.on("disconnect", () => {
         // get session variable for userId
         const { userId } = socket.data;
 
         // decrement connection count for user
-        if (userId && users[userId]) {
-          users[userId] -= 1;
+        if (userId && userCounts[userId]) {
+          userCounts[userId] -= 1;
 
-          console.log("set inactive", users, { reason });
-
-          if (users[userId] === 0) {
-            updateUser(userId, {
-              active: false,
-            });
-
-            delete users[userId];
+          if (userCounts[userId] === 0) {
+            delete userCounts[userId];
 
             updateActiveUsers();
           }
+        }
+      });
+
+      socket.on(
+        "steal a thing",
+        ({ victimId, thingId }: { victimId: string; thingId: string }) => {
+          console.log("steal a thing triggered");
+          socket.to(victimId).emit("thing stolen", thingId);
+        }
+      );
+
+      socket.on("user update", async () => {
+        const { userId } = socket.data;
+
+        try {
+          // check if user has any claims
+          await dbConnect();
+
+          const claimedThings = await Thing.find(
+            { claimedBy: { $eq: userId } },
+            // just get namespaces
+            "namespace"
+          ).exec();
+
+          const namespaces = Array.from(
+            new Set(claimedThings.map(({ namespace }) => namespace))
+          );
+
+          if (namespaces.length) {
+            refreshThings(namespaces);
+          }
+        } catch (e) {
+          console.error("failed to invalidate claimed things");
+          console.error(e);
+        }
+      });
+
+      socket.on("thing update", async (thingId: string) => {
+        try {
+          // check if user has any claims
+          await dbConnect();
+
+          const { namespace = "" } =
+            (await Thing.findById(thingId, "namespace").exec()) || {};
+
+          if (namespace) {
+            refreshThings([namespace]);
+          }
+        } catch (e) {
+          console.error("failed to invalidate updated things");
+          console.error(e);
         }
       });
     });
